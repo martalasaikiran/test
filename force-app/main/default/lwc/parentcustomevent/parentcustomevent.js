798d74b8f33f7bd693cde979fab5fe0dda96c209
martalasaikiran <martalasaikiranreddy1119@gmail.com>
2025-06-03 15:28:20 +0530

    change

--- force-app/main/default/lwc/parentcustomevent/parentcustomevent.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Parentcustomevent extends LightningElement {
-    display=false;
-
-    handlechildevent(){
-        this.display=true;
-   }
-}
